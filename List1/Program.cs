775d98558604e1c88a40a2d178de742a6e901919
asus <asus@DESKTOP-OTTG4TB>
2024-08-24 20:03:13 +0300

    Add project files.

---
+++ List1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace List1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> guests = new List<string>()
+            {
+                "Bülent ERSOY",
+                "Ajda PEKKAN",
+                "Ebru GÜNDEŞ",
+                "Hadise",
+                "Hande YENER",
+                "Tarkan",
+                "Funda ARAR",
+                "Demet AKALIN"
+            };
+
+            Console.WriteLine("Davetlilerin isimleri:");
+
+            foreach (string guest in guests)
+            {
+                Console.WriteLine(guest);
+            }
+        }
+    }
+}
